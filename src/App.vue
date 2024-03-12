c23a8f7790eed81ea8d1264905eebcea6cb18fef
Joshua Permito <dwigoric@insanedevs.com>
2024-03-12 22:55:39 +0800

    feat: add input for operands and round method choice

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
-<script setup></script>
+<script setup>
+// Import packages
+import { ref } from 'vue'
+
+// Refs
+const roundingMethod = ref(true)
+
+const binary1 = ref('')
+const binary2 = ref('')
+</script>
 
 <template>
     <div>
-        <h1>Hello, World!</h1>
+        <h2>Choose your rounding method:</h2>
+        <VSwitch id="rounding-method" v-model="roundingMethod">
+            <template #label>
+                <VDialogBottomTransition>
+                    <div v-if="roundingMethod">RTN-TE</div>
+                    <div v-else>G/R/S</div>
+                </VDialogBottomTransition>
+            </template>
+        </VSwitch>
     </div>
+
+    <div id="binary-input">
+        <h2>Binary numbers to add</h2>
+        <VTextField v-model="binary1" label="Binary 1" />
+        <VIcon icon="mdi-plus-circle"></VIcon>
+        <VTextField v-model="binary2" label="Binary 2" />
+    </div>
+
+    <div></div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#rounding-method {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+}
+</style>
