20ff4ab5bb710b10ce1f467bfbe17c9b3a57a926
Joshua Permito <dwigoric@insanedevs.com>
2024-03-18 08:14:22 +0800

    feat: add base-2 input

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <script setup>
 // Import packages
-import { ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
 
 // Refs
 const useGrs = ref(false)
 const rawInput = ref(false)
 
-const binary1 = ref('')
-const binary2 = ref('')
+const binary1 = ref('001111111' + '0'.repeat(23))
+const binary2 = ref('001111111' + '0'.repeat(23))
+const base2_maxDigits = ref(7)
+
+const base2_1 = reactive({
+    magnitude: '0000000',
+    exponent: 0,
+    sign: 0
+})
+
+const base2_2 = reactive({
+    magnitude: '0000000',
+    exponent: 0,
+    sign: 0
+})
 
 // Functions
 const checkBit = (e) => {
@@ -17,6 +30,33 @@ const checkBit = (e) => {
         }
     }
 }
+
+const checkExponent = (e) => {
+    if (e.inputType === 'insertText') {
+        if (e.data !== '-' && e.data !== '+' && isNaN(e.data)) {
+            e.preventDefault()
+        }
+    }
+}
+
+// Lifecycle hooks
+watch(base2_maxDigits, (newVal) => {
+    // Append 0 if the magnitude is shorter than the new max digits
+    if (base2_1.magnitude.length < newVal) {
+        base2_1.magnitude += '0'.repeat(newVal - base2_1.magnitude.length)
+    }
+    if (base2_2.magnitude.length < newVal) {
+        base2_2.magnitude += '0'.repeat(newVal - base2_2.magnitude.length)
+    }
+
+    // Truncate the magnitude if it's longer than the new max digits
+    if (base2_1.magnitude.length > newVal) {
+        base2_1.magnitude = base2_1.magnitude.slice(0, newVal)
+    }
+    if (base2_2.magnitude.length > newVal) {
+        base2_2.magnitude = base2_2.magnitude.slice(0, newVal)
+    }
+})
 </script>
 
 <template>
@@ -47,11 +87,61 @@ const checkBit = (e) => {
 
     <VExpandTransition>
         <div v-if="rawInput" id="binary-input">
-            <h2 class="mb-3">Binary numbers to add</h2>
+            <h2 class="mb-3">Binary numbers to add (s, e, m)</h2>
             <VOtpInput v-model="binary1" length="32" variant="underlined" @beforeinput="checkBit" />
             <VSpacer class="mt-5" />
             <VIcon icon="mdi-plus" style="align-self: center"></VIcon>
             <VOtpInput v-model="binary2" length="32" variant="underlined" @beforeinput="checkBit" />
+        </div>
+        <div v-else id="base2-input">
+            <div>
+                <h2>Max digits in the mantissa</h2>
+                <VSlider v-model="base2_maxDigits" max="23" min="1" step="1" thumb-label="always" />
+            </div>
+            <h2 class="mb-3">Base-2 to add</h2>
+            <div id="base2-fields">
+                <div class="base2-field bg-blue-darken-4">
+                    <VIcon>mdi-numeric-1</VIcon>
+                    <VIcon>mdi-circle-small</VIcon>
+                    <VOtpInput
+                        v-model="base2_1.magnitude"
+                        :length="base2_maxDigits"
+                        variant="underlined"
+                        @beforeinput="checkBit"
+                    />
+                    <VIcon>mdi-close</VIcon>
+                    <VIcon>mdi-numeric-2</VIcon>
+                    <VIcon>mdi-chevron-up</VIcon>
+                    <VTextField
+                        v-model.number="base2_1.exponent"
+                        hide-details
+                        single-line
+                        @beforeinput="checkExponent"
+                    />
+                </div>
+                <VSpacer class="mb-3" />
+                <VIcon icon="mdi-plus" style="align-self: center"></VIcon>
+                <VSpacer class="mt-3" />
+                <div class="base2-field bg-blue-darken-4">
+                    <VIcon>mdi-numeric-1</VIcon>
+                    <VIcon>mdi-circle-small</VIcon>
+                    <VOtpInput
+                        v-model="base2_2.magnitude"
+                        :length="base2_maxDigits"
+                        variant="underlined"
+                        @beforeinput="checkBit"
+                    />
+                    <VIcon>mdi-close</VIcon>
+                    <VIcon>mdi-numeric-2</VIcon>
+                    <VIcon>mdi-chevron-up</VIcon>
+                    <VTextField
+                        v-model.number="base2_2.exponent"
+                        hide-details
+                        single-line
+                        @beforeinput="checkExponent"
+                    />
+                </div>
+            </div>
         </div>
     </VExpandTransition>
 </template>
@@ -72,11 +162,49 @@ const checkBit = (e) => {
     width: 100%;
     display: flex;
     flex-flow: column nowrap;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: stretch;
 
     :deep(.v-otp-input__content) {
         max-width: 100%;
     }
+}
+
+#base2-input {
+    width: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-around;
+    align-items: stretch;
+}
+
+#base2-fields {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    font-size: 1.5rem;
+
+    :deep(.v-otp-input__content) {
+        max-width: 90%;
+    }
+}
+
+.base2-field {
+    margin: 0 1rem;
+    padding: 0 1rem;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    max-width: 100%;
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: bold;
+}
+
+.v-text-field {
+    width: 20rem;
 }
 </style>
