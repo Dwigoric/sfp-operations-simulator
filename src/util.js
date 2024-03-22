8d0f14f519143ec43763e19c98717795be9d8b7a
Graham Ogatia <63532129+grahamogatia@users.noreply.github.com>
2024-03-22 18:08:31 +0800

    feat: add function to convert mantissa to decimal

--- src/util.js
+++ src/util.js
@@ -16,59 +16,59 @@ const alignExponent = (inputOp1, inputOp2) => {
                 exponent: op2.exponent,
                 magnitude: '1.' + op2.mantissa
             }
-        };
+        }
     }
 
     // Initialize base integer to 1 for both operands
-    op1.integer = 1;
-    op2.integer = 1;
+    op1.integer = 1
+    op2.integer = 1
 
     // Normalize op1
     if (op1.mantissa != 0) {
         while (op1.mantissa[0] === '0' && op1.mantissa.length > 1) {
-            op1.mantissa = op1.mantissa.slice(1) + '0';
-            op1.exponent--;
+            op1.mantissa = op1.mantissa.slice(1) + '0'
+            op1.exponent--
         }
     }
 
     // Normalize op2
     if (op2.mantissa != 0) {
         while (op2.mantissa[0] === '0' && op2.mantissa.length > 1) {
-            op2.mantissa = op2.mantissa.slice(1) + '0';
-            op2.exponent--;
+            op2.mantissa = op2.mantissa.slice(1) + '0'
+            op2.exponent--
         }
     }
 
     // Adjust exponents to be equal
     while (op1.exponent < op2.exponent) {
-        op1.mantissa = '0' + op1.mantissa.slice(0, -1);
-        op1.exponent++;
+        op1.mantissa = '0' + op1.mantissa.slice(0, -1)
+        op1.exponent++
     }
     while (op2.exponent < op1.exponent) {
-        op2.mantissa = '0' + op2.mantissa.slice(0, -1);
-        op2.exponent++;
+        op2.mantissa = '0' + op2.mantissa.slice(0, -1)
+        op2.exponent++
     }
 
-    const op1Exp = Math.abs(inputOp1.exponent); // Taking absolute value
-    const op2Exp = Math.abs(inputOp2.exponent); // Taking absolute value
+    const op1Exp = Math.abs(inputOp1.exponent) // Taking absolute value
+    const op2Exp = Math.abs(inputOp2.exponent) // Taking absolute value
 
     if (op1Exp > op2Exp && op1.mantissa[0] === '0') {
-        op1.integer = 0;
+        op1.integer = 0
     } else if (op1Exp < op2Exp && op2.mantissa[0] === '0') {
-        op2.integer = 0;
+        op2.integer = 0
     }
 
     // Special Case: if op1.mantissa === 0 || op2.mantissa === 0
-    let diff = Math.abs(op1.exponent - op2.exponent);
+    let diff = Math.abs(op1.exponent - op2.exponent)
     if (op1.mantissa == 0) {
-        op1.mantissa = op1.mantissa.substring(0, diff - 1) + '1' + op1.mantissa.substring(diff);
+        op1.mantissa = op1.mantissa.substring(0, diff - 1) + '1' + op1.mantissa.substring(diff)
     }
     if (op2.mantissa == 0) {
-        op2.mantissa = op2.mantissa.substring(0, diff - 1) + '1' + op2.mantissa.substring(diff);
+        op2.mantissa = op2.mantissa.substring(0, diff - 1) + '1' + op2.mantissa.substring(diff)
     }
 
-    op1.magnitude = op1.integer + '.' + op1.mantissa;
-    op2.magnitude = op2.integer + '.' + op2.mantissa;
+    op1.magnitude = op1.integer + '.' + op1.mantissa
+    op2.magnitude = op2.integer + '.' + op2.mantissa
 
     return {
         op1: {
@@ -83,5 +83,20 @@ const alignExponent = (inputOp1, inputOp2) => {
         }
     }
 }
+const mantissaBinaryToDecimal = (mantissa) => {
+    let total = 0
+    for (let i = 0; i < mantissa.length; i++) {
+        const power = -(i + 1)
+        const bit = mantissa[i]
 
-export { alignExponent }
+        if (bit === 1) {
+            total += Math.power(2, power)
+        }
+    }
+
+    return total
+}
+
+const addOperands = (op1, op2) => {}
+
+export { alignExponent, addOperands }
