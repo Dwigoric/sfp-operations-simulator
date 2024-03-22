abb8f78051166be0ea73c709f61a86d3e111edde
Lloyd26 <ademic26@yahoo.com>
2024-03-22 23:22:48 +0800

    Fixed incorrect input operand used in alignExponent function

--- src/util.js
+++ src/util.js
@@ -3,72 +3,23 @@
 const alignExponent = (inputOp1, inputOp2) => {
     const { op1, op2 } = JSON.parse(JSON.stringify({ op1: inputOp1, op2: inputOp2 })) // Deep copy
 
-    // Special Case: Exponents are equal, don't normalize
-    if (op1.exponent === op2.exponent) {
-        return {
-            op1: {
-                sign: op1.sign,
-                exponent: op1.exponent,
-                magnitude: '1.' + op1.mantissa
-            },
-            op2: {
-                sign: op2.sign,
-                exponent: op2.exponent,
-                magnitude: '1.' + op2.mantissa
-            }
-        }
+    op1.integer = 1;
+    op2.integer = 1;
+
+    let diff = Math.abs(op1.exponent - op2.exponent);
+
+    if (op1.exponent < op2.exponent) {
+        op1.mantissa = shift(op1.mantissa, diff, op1.mantissa.length, op1.integer);
+        op1.exponent = op2.exponent;
+        op1.integer = 0;
+    } else if (op1.exponent > op2.exponent) {
+        op2.mantissa = shift(op2.mantissa, diff, op2.mantissa.length, op2.integer);
+        op2.exponent = op1.exponent;
+        op2.integer = 0;
     }
 
-    // Initialize base integer to 1 for both operands
-    op1.integer = 1
-    op2.integer = 1
-
-    // Normalize op1
-    if (op1.mantissa != 0) {
-        while (op1.mantissa[0] === '0' && op1.mantissa.length > 1) {
-            op1.mantissa = op1.mantissa.slice(1) + '0'
-            op1.exponent--
-        }
-    }
-
-    // Normalize op2
-    if (op2.mantissa != 0) {
-        while (op2.mantissa[0] === '0' && op2.mantissa.length > 1) {
-            op2.mantissa = op2.mantissa.slice(1) + '0'
-            op2.exponent--
-        }
-    }
-
-    // Adjust exponents to be equal
-    while (op1.exponent < op2.exponent) {
-        op1.mantissa = '0' + op1.mantissa.slice(0, -1)
-        op1.exponent++
-    }
-    while (op2.exponent < op1.exponent) {
-        op2.mantissa = '0' + op2.mantissa.slice(0, -1)
-        op2.exponent++
-    }
-
-    const op1Exp = Math.abs(inputOp1.exponent) // Taking absolute value
-    const op2Exp = Math.abs(inputOp2.exponent) // Taking absolute value
-
-    if (op1Exp > op2Exp && op1.mantissa[0] === '0') {
-        op1.integer = 0
-    } else if (op1Exp < op2Exp && op2.mantissa[0] === '0') {
-        op2.integer = 0
-    }
-
-    // Special Case: if op1.mantissa === 0 || op2.mantissa === 0
-    let diff = Math.abs(op1.exponent - op2.exponent)
-    if (op1.mantissa == 0) {
-        op1.mantissa = op1.mantissa.substring(0, diff - 1) + '1' + op1.mantissa.substring(diff)
-    }
-    if (op2.mantissa == 0) {
-        op2.mantissa = op2.mantissa.substring(0, diff - 1) + '1' + op2.mantissa.substring(diff)
-    }
-
-    op1.magnitude = op1.integer + '.' + op1.mantissa
-    op2.magnitude = op2.integer + '.' + op2.mantissa
+    op1.magnitude = op1.integer + "." + op1.mantissa;
+    op2.magnitude = op2.integer + "." + op2.mantissa;
 
     return {
         op1: {
@@ -81,7 +32,22 @@ const alignExponent = (inputOp1, inputOp2) => {
             exponent: op2.exponent,
             magnitude: op2.magnitude
         }
+    };
+}
+
+const shift = (str, offset, len, int) => {
+    offset = Math.min(offset, len);
+    let str_shift = "";
+    for (let i = 0; i < offset; i++) {
+        if (i + 1 === offset) {
+            str_shift += int;
+            continue;
+        }
+        str_shift += '0';
     }
+    str_shift += str.substring(0, len - offset);
+
+    return str_shift;
 }
 
 const mantissaBinaryToDecimal = (mantissa) => { // 1010101
